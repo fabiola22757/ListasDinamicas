3618e9e005e7cf4a350a9e11c2bda22f22028f3e
Fabiola <fabiola.juarez>
2025-10-15 17:47:39 -0600

    Initial Commit

---
+++ Listas/ListasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListasApp.swift
+//  Listas
+//
+//  Created by win603 on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ListasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
